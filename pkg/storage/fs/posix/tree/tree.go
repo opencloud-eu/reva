5f2b717dfb3f1d8952257639fcd1180a17cd4a24
André Duffeck <a.duffeck@opencloud.eu>
2026-04-20 08:33:23 +0200

    Adjust tracer names

--- pkg/storage/fs/posix/tree/tree.go
+++ pkg/storage/fs/posix/tree/tree.go
@@ -68,7 +68,7 @@ var (
 )
 
 func init() {
-	tracer = otel.Tracer("github.com/cs3org/reva/pkg/storage/pkg/decomposedfs/tree")
+	tracer = otel.Tracer("github.com/opencloud-eu/reva/v2/pkg/storage/fs/posix/tree")
 }
 
 type Watcher interface {
