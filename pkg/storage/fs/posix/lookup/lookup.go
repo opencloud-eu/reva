2de23a9ca218d25581d12e8a8ea4268d523e11a2
André Duffeck <a.duffeck@opencloud.eu>
2026-05-18 15:52:16 +0200

    Fall back to disk if ids aren't available from the cache

--- pkg/storage/fs/posix/lookup/lookup.go
+++ pkg/storage/fs/posix/lookup/lookup.go
@@ -118,6 +118,11 @@ func (lu *Lookup) IDsForPath(ctx context.Context, path string) (string, string, 
 	// IDsForPath returns the space and opaque id for the given path
 	spaceID, nodeID, err := lu.IDCache.GetByPath(ctx, path)
 	if err != nil {
+		// fallback to disk
+		sID, nID, _, _, mErr := lu.metadataBackend.IdentifyPath(ctx, path)
+		if mErr == nil && nID != "" {
+			return sID, nID, nil
+		}
 		return "", "", err
 	}
 	return spaceID, nodeID, nil
