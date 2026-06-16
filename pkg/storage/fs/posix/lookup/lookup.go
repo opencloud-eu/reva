dd25cf24f6f89165c745d861cf02ca91762ea76f
André Duffeck <a.duffeck@opencloud.eu>
2026-06-16 09:09:41 +0200

    Introduce a constant for the trash dir name

--- pkg/storage/fs/posix/lookup/lookup.go
+++ pkg/storage/fs/posix/lookup/lookup.go
@@ -47,7 +47,10 @@ import (
 
 var tracer trace.Tracer
 
-const MetadataDir = ".oc-nodes"
+const (
+	MetadataDir = ".oc-nodes"
+	TrashDir    = ".Trash"
+)
 
 var _spaceTypePersonal = "personal"
 var _spaceTypeProject = "project"
